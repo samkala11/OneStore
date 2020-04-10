1761d46625ec2f490840f4892ba7eb47d9186347
sam <sam>
2020-04-10 01:17:08 -0700

    add email 4

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,7 @@ if Rails.env.production?
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
+      :ssl            => true,
       :user_name      => ENV['SENDGRID_USERNAME'],
       :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
